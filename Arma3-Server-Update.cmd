0992d5521b25a56f37d821cc61e552ecb795132a
Canuck Brian <canuckbrian@users.noreply.github.com>
2020-04-26 09:37:15 -0700

    Create Arma3-Server-Update.cmd

---
+++ Arma3-Server-Update.cmd
@@ -0,0 +1,26 @@
+@echo off
+@rem http://media.steampowered.com/installer/steamcmd.zip
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+	:: DEFINE the following variables where applicable to your install
+
+	SET STEAMLOGIN=packet_loss_server w8r9-2Fd
+	SET serverID=233780 -beta
+
+	SET serverPath=C:\Servers\Arma3
+		SET STEAMPATH=C:\Servers\SteamCMD
+
+:: _________________________________________________________
+
+echo.
+echo     You are about to install/update Arma 3 server
+echo        Dir: %serverPath%
+echo        Branch: %serverID%
+echo.
+echo     Key "ENTER" to proceed
+pause
+%STEAMPATH%\steamcmd.exe +login %STEAMLOGIN% +force_install_dir %serverPath% +"app_update %serverID%" validate +quit
+echo .
+echo     Your Arma 3 server is now installed / up to date
+echo     key "ENTER" to exit
+pause
